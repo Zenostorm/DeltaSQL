b8e22cc1b53aa7c6b618a007b148e797d23c5afe
Zeno <z3n0st0rm@gmail.com>
2025-06-15 15:25:40 +1200

    15/06/2025
    >Changed magazine names in delta.db
    >Helmets no longer show visor options tab if there is none
    >Added scav king crown to helmets

--- Flask/routes.py
+++ Flask/routes.py
@@ -119,7 +119,7 @@ def all_helmets():
     
     results = cur.fetchall()
     conn.close()
-    return render_template('helmets.html', params=results, title="Helmet", search=search_query, easter_egg_queries=easter_egg_queries)
+    return render_template('helmets.html', params=results, title="Helmets", search=search_query, easter_egg_queries=easter_egg_queries)
 
 @app.route("/helmet/<int:id>")
 def helmet(id):
